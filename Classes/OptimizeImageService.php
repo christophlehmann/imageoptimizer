f69b55333d67df86dbe6691d0f5e349d4039f6f8
Daniela Grammlich <grammlich@punkt.de>
2017-02-22 10:22:33 +0100

    TASK: #3 Make parameter in jpegoptim command ore descriptive

--- Classes/OptimizeImageService.php
+++ Classes/OptimizeImageService.php
@@ -48,7 +48,7 @@ class OptimizeImageService {
 			if (is_string($binary)) {
 				$jpegoptimQuality = (integer)$this->configuration['jpegoptimQuality'];
 				$quality = ($jpegoptimQuality > 0 && $jpegoptimQuality <= 100) ? $jpegoptimQuality : 90;
-				$command = sprintf('%s -m %s -s --all-progressive %s 2>&1', $binary, $quality, $file);
+				$command = sprintf('%s -m %s --strip-all --all-progressive %s 2>&1', $binary, $quality, $file);
 			}
 		}
 
