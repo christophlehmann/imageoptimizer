b64f7bd7ab864b9e9a9f67b1c5f2a7ce625c0c3d
Daniel <revoltek-daniel@users.noreply.github.com>
2018-06-07 10:13:10 +0200

    Task: Add Support for TYPO3 v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.4',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-8.9.99',
+			'typo3' => '6.2.0-9.9.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
