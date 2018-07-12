7f56eb6983e664c58a48faed740d0afe3eb724e8
Sascha Egerer <info@sascha-egerer.de>
2018-07-12 18:10:28 +0200

    [CLEANUP] Reformat code in PSR2

--- Classes/OptimizeImageService.php
+++ Classes/OptimizeImageService.php
@@ -3,56 +3,59 @@ namespace Lemming\Imageoptimizer;
 
 use TYPO3\CMS\Core\Utility\CommandUtility;
 
-class OptimizeImageService {
+class OptimizeImageService
+{
 
-	/**
-	 * Initialize
-	 */
-	public function __construct() {
-		$this->configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imageoptimizer']);
-	}
+    /**
+     * Initialize
+     */
+    public function __construct()
+    {
+        $this->configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imageoptimizer']);
+    }
 
-	/**
-	 * Perform image optimization
-	 *
-	 * @param string $file
-	 * @param string $extension
-	 * @param boolean $fileIsUploaded
-	 */
-	public function process($file, $extension = NULL, $fileIsUploaded = FALSE) {
-		if ($extension === NULL) {
-			$pathinfo = pathinfo($file);
-			if ($pathinfo['extension'] !== NULL) {
-				$extension = $pathinfo['extension'];
-			}
-		}
-		$extension = strtolower($extension);
-		if ($extension == 'jpeg') {
-			$extension = 'jpg';
-		}
-		$when = $fileIsUploaded === TRUE ? 'Upload' : 'Processing';
+    /**
+     * Perform image optimization
+     *
+     * @param string $file
+     * @param string $extension
+     * @param bool $fileIsUploaded
+     */
+    public function process($file, $extension = null, $fileIsUploaded = false)
+    {
+        if ($extension === null) {
+            $pathinfo = pathinfo($file);
+            if ($pathinfo['extension'] !== null) {
+                $extension = $pathinfo['extension'];
+            }
+        }
+        $extension = strtolower($extension);
+        if ($extension == 'jpeg') {
+            $extension = 'jpg';
+        }
+        $when = $fileIsUploaded === true ? 'Upload' : 'Processing';
 
-		if ((bool)$this->configuration[$extension . 'On' . $when] === FALSE) {
-			return;
-		}
+        if ((bool)$this->configuration[$extension . 'On' . $when] === false) {
+            return;
+        }
 
-		$binary = CommandUtility::getCommand(escapeshellcmd($this->configuration[$extension . 'Binary']));
+        $binary = CommandUtility::getCommand(escapeshellcmd($this->configuration[$extension . 'Binary']));
 
-		if (!is_string($binary)) {
-			throw new \RuntimeException('Binary ' . $binary . ' not found', 1488631746);
-		}
+        if (!is_string($binary)) {
+            throw new \RuntimeException('Binary ' . $binary . ' not found', 1488631746);
+        }
 
-		$parameters = $this->configuration[$extension . 'ParametersOn' . $when];
-		$parameters = preg_replace('/[^A-Za-z0-9-%: =]/', "", $parameters);
-		$parameters = preg_replace('/%s/', $file, $parameters);
+        $parameters = $this->configuration[$extension . 'ParametersOn' . $when];
+        $parameters = preg_replace('/[^A-Za-z0-9-%: =]/', '', $parameters);
+        $parameters = preg_replace('/%s/', $file, $parameters);
 
-		$command = $binary . ' ' . $parameters . ' 2>&1';
-		$output = [];
-		$returnValue = 0;
-		CommandUtility::exec($command, $output, $returnValue);
-		if ((bool)$this->configuration['debug'] === TRUE && is_object($GLOBALS['BE_USER'])) {
-			$error = $returnValue == 0 ? 0 : 1;
-			$GLOBALS['BE_USER']->writelog(4, 0, $error, 0, $command . ' exited with ' . $returnValue . '. Output was: ' . implode(' ', $output), $output);
-		}
-	}
+        $command = $binary . ' ' . $parameters . ' 2>&1';
+        $output = [];
+        $returnValue = 0;
+        CommandUtility::exec($command, $output, $returnValue);
+        if ((bool)$this->configuration['debug'] === true && is_object($GLOBALS['BE_USER'])) {
+            $error = $returnValue == 0 ? 0 : 1;
+            $GLOBALS['BE_USER']->writelog(4, 0, $error, 0, $command . ' exited with ' . $returnValue . '. Output was: ' . implode(' ', $output), $output);
+        }
+    }
 }
--- Classes/StatusReport.php
+++ Classes/StatusReport.php
@@ -4,41 +4,44 @@ namespace Lemming\Imageoptimizer;
 use TYPO3\CMS\Core\Utility\CommandUtility;
 use TYPO3\CMS\Reports\Status;
 
-class StatusReport implements \TYPO3\CMS\Reports\StatusProviderInterface {
+class StatusReport implements \TYPO3\CMS\Reports\StatusProviderInterface
+{
 
-	/**
-	 * @var \TYPO3\CMS\Extbase\Object\ObjectManager
-	 */
-	protected $objectManager = null;
+    /**
+     * @var \TYPO3\CMS\Extbase\Object\ObjectManager
+     */
+    protected $objectManager = null;
 
-	/**
-	 * Default constructor
-	 */
-	public function __construct() {
-		$this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
-	}
+    /**
+     * Default constructor
+     */
+    public function __construct()
+    {
+        $this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
+    }
 
-	/**
-	 * Determines if the needed binaries are found
-	 *
-	 * @return array List of statuses
-	 */
-	public function getStatus() {
-		$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imageoptimizer']);
-		$extensions = ['jpg', 'png', 'gif', 'svg'];
-		foreach ($extensions as $extension) {
-			$binary = escapeshellcmd($configuration[$extension . 'Binary']);
-			$binaryFound = is_string(CommandUtility::getCommand($binary));
-			$binaryUsed = ((bool)($configuration[$extension . 'OnUpload']) === TRUE || (bool)($configuration[$extension . 'OnProcessing']) === TRUE);
+    /**
+     * Determines if the needed binaries are found
+     *
+     * @return array List of statuses
+     */
+    public function getStatus()
+    {
+        $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imageoptimizer']);
+        $extensions = ['jpg', 'png', 'gif', 'svg'];
+        foreach ($extensions as $extension) {
+            $binary = escapeshellcmd($configuration[$extension . 'Binary']);
+            $binaryFound = is_string(CommandUtility::getCommand($binary));
+            $binaryUsed = ((bool)($configuration[$extension . 'OnUpload']) === true || (bool)($configuration[$extension . 'OnProcessing']) === true);
 
-			$status[$extension] = $this->objectManager->get(
-				Status::class,
-				'Binary ' . $binary,
-				$binaryFound ? 'Found' : 'Not found',
-				$binaryUsed ? 'In use' : 'Not in use',
-				$binaryFound || $binaryUsed === FALSE ? Status::OK : Status::ERROR
-			);
-		}
-		return $status;
-	}
+            $status[$extension] = $this->objectManager->get(
+                Status::class,
+                'Binary ' . $binary,
+                $binaryFound ? 'Found' : 'Not found',
+                $binaryUsed ? 'In use' : 'Not in use',
+                $binaryFound || $binaryUsed === false ? Status::OK : Status::ERROR
+            );
+        }
+        return $status;
+    }
 }
