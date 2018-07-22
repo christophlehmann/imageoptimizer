daf64e007c99e9db72c8410425e8939bdadb3b80
Christoph Lehmann <post@christophlehmann.eu>
2018-07-22 23:18:35 +0200

    Task: Raise TYPO3 dependency to >= 9.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '9.4.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
