2fb95b3744fa48af399e935f83ac6b5171cf71e3
Sascha Egerer <info@sascha-egerer.de>
2018-07-23 13:15:33 +0200

    [TASK] Check file exists before processing
    The physical file could already have been deleted so check
    if it exists before we try to process it.

--- Classes/FileAspects.php
+++ Classes/FileAspects.php
@@ -50,6 +50,10 @@ class FileAspects
      */
     public function processFile($fileProcessingService, $driver, $processedFile)
     {
+        if (!$processedFile->exists()) {
+            return;
+        }
+
         if ($processedFile->usesOriginalFile() === true || $processedFile->isUpdated() === true) {
             $fileForLocalProcessing = $processedFile->getForLocalProcessing();
             $this->service->process($fileForLocalProcessing, $processedFile->getExtension());
