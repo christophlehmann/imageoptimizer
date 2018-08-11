58029911b56df18a58271083befcd7c478ceb460
Christoph Lehmann <post@christophlehmann.eu>
2018-08-11 02:24:01 +0200

    Use class constants instead of strings

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['imageoptimizer']);
 
 if (!isset($signalSlotDispatcher)) {
-    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\SignalSlot\Dispatcher');
+    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 }
 
 if ((isset($configuration['jpgOnUpload']) && (bool)$configuration['jpgOnUpload'] === true) ||
@@ -11,16 +11,16 @@ if ((isset($configuration['jpgOnUpload']) && (bool)$configuration['jpgOnUpload']
     (isset($configuration['gifOnUpload']) && (bool)$configuration['gifOnUpload'] === true) ||
     (isset($configuration['svgOnUpload']) && (bool)$configuration['svgOnUpload'] === true)) {
     $signalSlotDispatcher->connect(
-        'TYPO3\\CMS\\Core\\Resource\\ResourceStorage',
+        \TYPO3\CMS\Core\Resource\ResourceStorage::class,
         \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileAdd,
-        'Lemming\\Imageoptimizer\\FileAspects',
+        \Lemming\Imageoptimizer\FileAspects::class,
         'addFile'
     );
 
     $signalSlotDispatcher->connect(
-        'TYPO3\\CMS\\Core\\Resource\\ResourceStorage',
+        \TYPO3\CMS\Core\Resource\ResourceStorage::class,
         \TYPO3\CMS\Core\Resource\ResourceStorageInterface::SIGNAL_PreFileReplace,
-        'Lemming\\Imageoptimizer\\FileAspects',
+        \Lemming\Imageoptimizer\FileAspects::class,
         'replaceFile'
     );
 }
@@ -29,9 +29,9 @@ if ((isset($configuration['jpgOnProcessing']) && (bool)$configuration['jpgOnProc
     (isset($configuration['pngOnProcessing']) && (bool)$configuration['pngOnProcessing'] === true) ||
     (isset($configuration['gifOnProcessing']) && (bool)$configuration['gifOnProcessing'] === true)) {
     $signalSlotDispatcher->connect(
-        'TYPO3\\CMS\\Core\\Resource\\ResourceStorage',
+        \TYPO3\CMS\Core\Resource\ResourceStorage::class,
         \TYPO3\CMS\Core\Resource\Service\FileProcessingService::SIGNAL_PostFileProcess,
-        'Lemming\\Imageoptimizer\\FileAspects',
+        \Lemming\Imageoptimizer\FileAspects::class,
         'processFile'
     );
 }
