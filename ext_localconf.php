cbb9fba4a956cd0285d0b5718ad3e96596bdc679
Igor Neyman <in@points.de>
2023-07-26 14:09:54 +0200

    fix: add missing namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 
 defined('TYPO3') or die();
 
-$typo3Version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(Typo3Version::class);
+$typo3Version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
 if($typo3Version->getMajorVersion() < 12) {
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['ImageOptimizer'][] =
     \Lemming\Imageoptimizer\StatusReport::class;
